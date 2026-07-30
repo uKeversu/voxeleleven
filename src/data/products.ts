692dddbf08fd32eac8701854c25c51ad08009c6a
Kelwin Gameiro <kelwingameiro@gmail.com>
2026-07-30 07:52:14 -0300

    fix: ajuste de estoque

--- src/data/products.ts
+++ src/data/products.ts
@@ -301,7 +301,7 @@ export const products: Product[] = [
         description:
             "Modelo premium alternativo do Flamengo 2025",
         featured: true,
-        stock: estoque(0, 0, 1, 0)
+        stock: estoque(0, 0, 0, 0)
     },
 
     {
